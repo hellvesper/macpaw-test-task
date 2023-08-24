4bd40ef2dacf585c976f1e286829c94301dc3c94
Vespertilio Sh <hellvesper@gmail.com>
2023-08-24 17:28:12 +0300

    Initial Commit

---
+++ ruswarlosses/ruswarlossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ruswarlossesApp.swift
+//  ruswarlosses
+//
+//  Created by Виталий Шандуренко on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ruswarlossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
